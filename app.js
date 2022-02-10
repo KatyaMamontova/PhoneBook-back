2290aab21555354782000af68a4ac614858f1b40
KatyaMamontova <rbkm572@gmail.com>
2022-02-11 01:20:35 +0400

    added functions: addNumber, deleteNumber

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { send } = require('process');
+//const { send } = require('process');
 const app = express();
 const port = 3000;
 
@@ -9,104 +9,124 @@ app.get('/bla/:man/:woman', (req, res) => {     //http://localhost:3000/bla/pety
     res.send('ok');
 });
 
-const phones = [
+const numbers = [
     {
         name: 'Scarlet',
-        phone: '89128762312'
+        number: '89128762312'
     },
     {
         name: 'Grandma',
-        phone: '89227663510'
+        number: '89227663510'
     },
     {
         name: 'Santa',
-        phone: '89070064147'
+        number: '89070064147'
     },
     {
         name: 'Michael Scarn',
-        phone: '87179001234'
+        number: '87179001234'
     },
     {
         name: 'guy',
-        phone: '89198002143'
+        number: '89198002143'
     },
     {
         name: 'Jesus',
-        phone: '89102533123'
+        number: '89102533123'
     },
     {
         name: 'plumber',
-        phone: '89122583100'
+        number: '89122583100'
     },
 ];
 
-app.get('/phoneBook', (req, res) => {
+app.get('/numberBook', (req, res) => {
     /* res.send(
-        phones.forEach(elem => {
+        numbers.forEach(elem => {
             elem.name;
         })
     ); */
     let list = '';
-    phones.forEach(elem => {
-        list += `${elem.name}: ${elem.phone} <br>`;
+    numbers.forEach(elem => {
+        list += `${elem.name}: ${elem.number} <br>`;
     })
     res.send(list);
 });
 
-function validatePhone(phone) {
-    if (
-        ((phone.length === 11) && (phone.startsWith('89'))) ||
-        ((phone.length === 12) && (phone.startsWith('+79')))
-    ) return true;
-}
-
-function convertStrToPhone(str) {
-    let convertedPhone = '';
-    if (str[0] === '+') convertedPhone += '+';
-    const allowedSymbols = ['(', ')', '-'];
-    for (let i = 0; i < str.length; i++) {
-        /* if ((str[i] - 0)
-            || str[i] === '0'
-            || allowedSymbols.indexOf(str[i]) != -1
-        ) {
-            if ((str[i] - 0)
-                || str[i] === '0') {
-                convertedPhone += str[i];
-            }
-        } else return false; */
-        if ((str[i] - 0)
-            || str[i] === '0') {
-            convertedPhone += str[i];
-        } else if (allowedSymbols.indexOf(str[i]) != -1) {
-            convertedPhone += '';
-        } else return false;
-    }
-    /* if (str[0] === '+') convertedPhone += '+';
-    for (let i = 0; i < str.length; i++) {
-        if ((str[i] - 0) || str[i] === '0') convertedPhone += str[i];
-    } */
-    return convertedPhone;
-}
-
-app.get('/phoneBook/validate/:phone', (req, res) => {
-    let phone = req.params.phone;
-    let convertedPhone = convertStrToPhone(phone);
-    if (convertedPhone) {
-        if (validatePhone(convertedPhone)) {
-            res.send('valid');
-        } else res.send('invalid');
+app.get('/phoneBook/addNumber/:name/:number', (req, res) => {
+    let name = req.params.name;
+    let number = req.params.number;
+    let convertedNumber = convertStrToNumber(number);
+    if (convertedNumber) {
+        if (validateNumber(convertedNumber)) {
+            addNumber(name, phone);
+            res.send(`Создан новый контакт: ${name}, ${number}`);
+        } else res.send('invalid number number');
     } else res.send('incorrect input');
 });
 
+function addNumber(name, number) {
+    numbers.push({
+        name,
+        number
+    });
+}
 
-app.get('/bla', (req, res) => {//это запрос с названием bla гетовый 
-    console.log(req.query);
-    res.send('ok');
+app.get('/phoneBook/deleteNumber/:name', (req, res) => {
+    let name = req.params.name;
+    if (name) {
+        deleteNumber(name);
+        res.send('Контакт удален');
+    } else res.send('Контакт с таким именем не найден');
 });
 
-app.get('/*', (req, res) => {// /* будет отвечать на все запросы 
-    i++;
-    res.send(`ля ля ля ${i}`);
+function deleteNumber(name) {
+    numbers.forEach(elem => { if (elem.name === name) elem = {}; });
+}
+
+
+function convertStrToNumber(str) {
+    let convertedNumber = '';
+    let start = 0;
+    if (str[0] === '+') {
+        convertedNumber += '+';
+        start = 1;
+    };
+    const allowedSymbols = ['(', ')', '-'];
+    for (let i = start; i < str.length; i++) {
+        if ((str[i] - 0)
+            || str[i] === '0') {
+            convertedNumber += str[i];
+        } else if (allowedSymbols.indexOf(str[i]) != -1) {
+            convertedNumber += '';
+        } else return false;
+    }
+
+    //этот код допускает ввод номера телефона вообще с любыми цифрами
+    //просто вычленяет цифры из строки
+    /*if (str[0] === '+') convertedNumber += '+';
+    for (let i = 0; i < str.length; i++) {
+        if ((str[i] - 0) || str[i] === '0') convertedNumber += str[i];
+    } */
+
+    return convertedNumber;
+}
+
+function validateNumber(number) {
+    if (
+        ((number.length === 11) && (number.startsWith('89'))) ||
+        ((number.length === 12) && (number.startsWith('+79')))
+    ) return true;
+}
+
+app.get('/phoneBook/validate/:number', (req, res) => {
+    let number = req.params.number;
+    let convertedNumber = convertStrToNumber(number);
+    if (convertedNumber) {
+        if (validateNumber(convertedNumber)) {
+            res.send('valid');
+        } else res.send('invalid');
+    } else res.send('incorrect input');
 });
 
 app.listen(port, () => console.log('все ок, работаем'));
